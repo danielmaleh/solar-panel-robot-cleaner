309d180c02309d44148c6be6de5b9342f65a2ca0
GaranceBo <garance.boesinger@orange.fr>
2023-12-12 15:08:27 +0100

    Regarde

--- TESTS/test_buttons_current_motors.ino
+++ TESTS/test_buttons_current_motors.ino
@@ -1,4 +1,6 @@
 #include <Stepper.h>
+#include <AccelStepper.h>
+#define MotorInterfaceType 4
 //Motor driver pins
 //Driver 1 : brush DC motor & gearbox DC motor + PWM, jumpers removed for PWM
 //Gearbox motor
@@ -18,7 +20,8 @@ const int motorPin8 = 27;
 const int motorPin9 = 28; 
 const int motorPin10 = 29; 
 const int steps_per_rev = 200;
-Stepper our_stepper(steps_per_rev, motorPin7, motorPin8, motorPin9, motorPin10);
+//Stepper our_stepper(steps_per_rev, motorPin7, motorPin8, motorPin9, motorPin10);
+AccelStepper our_stepper = AccelStepper(MotorInterfaceType, motorPin7, motorPin8, motorPin9, motorPin10);
 
 //Current sensors (x3) Pins 
 const int currentPin1 = A1; 
@@ -50,7 +53,8 @@ void setup() {
   pinMode(motorPin5, OUTPUT); 
   pinMode(motorPin6, OUTPUT); 
   //Stepper
-  our_stepper.setSpeed(60); //Rev per min 
+  //our_stepper.setSpeed(60); //Rev per min 
+  our_stepper.setMaxSpeed(1000);
   //Set button pins as inputs
   pinMode(ButtonPin1, INPUT);
   pinMode(ButtonPin2, INPUT);
@@ -71,16 +75,24 @@ void loop() {
   //3) Set gearbox motor up until chariot switch on 
   //4) Set stepper motor rigth until end course switch on 
 
+  //Stepper motor control
+  //our_stepper.step(steps_per_rev);
+  //delay(3000);
+
+  //Stepper position control 
+  our_stepper.setCurrentPosition(0);
+  // Run the motor forward at 200 steps/second until the motor reaches 400 steps (2 revolutions):
+  while (our_stepper.currentPosition() != 400)  {
+    our_stepper.setSpeed(200);
+    our_stepper.runSpeed();
+  }
+  our_stepper.setSpeed(0); 
+  our_stepper.setCurrentPosition(0);
+
   // Command of a DC motor with PWM
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, HIGH);
   analogWrite(motorPin3, motorSpeed1);
-
-  //Stepper motor control
-  our_stepper.step(steps_per_rev);
-  delay(500);
-  our_stepper.step(-steps_per_rev);
-  delay(500);
 
   // Getting a current value from current sensor
   currentVal1 = analogRead(currentPin1);
@@ -89,19 +101,23 @@ void loop() {
   Serial.print(currentVal1, 5);
   Serial.println(" [A]");
 
-  delay(3000); // Short delay for debounce and sensor stability
+  delay(3000); // Short delay for debounce and sensor stability 
+}
 
-  // Reading an end switch state 
-  float currentButtonVal = analogRead(ButtonPin1);
-  if (currentButtonVal != lastButtonVal1) {
-    lastDebounceTime = millis();
-  }
+void check_switches() {
+  float currentButtonVal1 = analogRead(ButtonPin1);
+  float currentButtonVal2 = analogRead(ButtonPin2);
+  float currentButtonVal3 = analogRead(ButtonPin3);
+  lastDebounceTime = millis();
   if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (currentButtonVal != lastButtonVal1) {
-      lastButtonVal1 = currentButtonVal;
-      Serial.print("Button value: "); 
-      Serial.println(lastButtonVal1, 5); // released = 1023 and pressed = 0
+    if (currentButtonVal1 != lastButtonVal1) {
+      lastButtonVal1 = currentButtonVal1; // released = 1023 and pressed = 0
+    }
+    if (currentButtonVal2 != lastButtonVal2) {
+      lastButtonVal1 = currentButtonVal2; // released = 1023 and pressed = 0
+    }
+    if (currentButtonVal3 != lastButtonVal3) {
+      lastButtonVal1 = currentButtonVal3; // released = 1023 and pressed = 0
     }
   }
-  delay(100); 
 }
