fe005839c3786aea5775f9152d98acb62f8a2493
Dani <Dani>
2023-12-21 10:19:41 +0100

    test

--- PROJET_PDSE/test.ino
+++ PROJET_PDSE/test.ino
@@ -22,77 +22,97 @@ void setup() {
     Serial.println("SETUP7");
 }
 
+// UP,
+// WATER_FILLING,
+// LEFT,
+// PROBLEM,
+// RIGHT,
+// REST,
+// DOWN,
+// RETURN_HOME,
+// BRUSH,
+// TRANSLATION,
+// VALVE,
+// BUTTONR,
+// BUTTONCHOME,
+// BUTONC1,
+// IR,
+// RAIN,
+// CURRENT_PIN1,
+// CURRENT_PIN2
+
 // Main loop
 void loop() {
+    delay(5000);
     Serial.println("LOOP");
     moveMotor(UP, 220);
-    Serial.println("moveMotor1"); // do not work
-    delay(1500);
+    Serial.println("moveMotor1"); 
+    delay(1000);
     stopAllMotors();
     Serial.println("stopAllMotors1");
-    delay(1500);
-    // updateLEDs(WATER_FILLING);
-    // Serial.println("updateLEDs1");
-    // delay(1500);
-    // moveMotor(LEFT, 5);
-    // Serial.println("moveMotor3");
-    // delay(1500);
-    // stopAllMotors();
-    // Serial.println("stopAllMotors3");
-    // delay(1500);
-    // updateLEDs(PROBLEM);
-    // Serial.println("updateLEDs2"); 
-    // delay(1500);
-    // moveMotor(RIGHT, 5);
-    // Serial.println("moveMotor4");
-    // delay(1500);
-    // stopAllMotors();
-    // Serial.println("stopAllMotors4");
-    // delay(1500);
-    // updateLEDs(REST);
-    // Serial.println("updateLEDs3");
-    // delay(1500);
-    // moveMotor(DOWN, 220);
-    // Serial.println("moveMotor2");
-    // delay(1500);
-    // stopAllMotors();
-    // Serial.println("stopAllMotors2");
-    // delay(1500);
-    // updateLEDs(RETURN_HOME);
-    // Serial.println("updateLEDs4");
-    // delay(1500);
-    // controlBrushMotor(true, 230); // what is happening?
-    // Serial.println("controlBrushMotor"); 
-    // delay(1500);
-    // updateLEDs(TRANSLATION);
-    // Serial.println("updateLEDs5");
-    // delay(1500);
-    // controlValve(0);
-    // Serial.println("controlValve");
-    // delay(1500);
-    // controlValve(180);
-    // Serial.println("controlValve");
-    // delay(1500);
-    // checkButtonR();
-    // Serial.println("checkButtonR");
-    // delay(1500);
-    // checkButtonC1();
-    // Serial.println("checkButtonC1");
-    // delay(1500);
-    // checkButtonC2();
-    // Serial.println("checkButtonC2");
-    // delay(1500);
-    // checkIRSensor();
-    // Serial.println("checkIRSensor");
-    // delay(1500);
-    // checkRainSensor();
-    // Serial.println("checkRainSensor");
-    // delay(1500);
-    // checkCurrent(CURRENT_PIN1);
-    // Serial.println("checkCurrent1");
-    // delay(1500);
-    // checkCurrent(CURRENT_PIN2);
-    // Serial.println("checkCurrent2");
-    // delay(1500);
+    delay(1000);
+    updateLEDs(WATER_FILLING);
+    Serial.println("updateLEDs1");
+    delay(1000);
+    moveMotor(LEFT, 5);
+    Serial.println("moveMotor3");
+    delay(1000);
+    stopAllMotors();
+    Serial.println("stopAllMotors3");
+    delay(1000);
+    updateLEDs(PROBLEM);
+    Serial.println("updateLEDs2"); 
+    delay(1000);
+    moveMotor(RIGHT, 5);
+    Serial.println("moveMotor4");
+    delay(1000);
+    stopAllMotors();
+    Serial.println("stopAllMotors4");
+    delay(1000);
+    updateLEDs(REST);
+    Serial.println("updateLEDs3");
+    delay(1000);
+    moveMotor(DOWN, 220);
+    Serial.println("moveMotor2");
+    delay(1000);
+    stopAllMotors();
+    Serial.println("stopAllMotors2");
+    delay(1000);
+    updateLEDs(RETURN_HOME);
+    Serial.println("updateLEDs4");
+    delay(1000);
+    controlBrushMotor(true, 230); // what is happening? what direction?
+    Serial.println("controlBrushMotor"); 
+    delay(1000);
+    updateLEDs(TRANSLATION);
+    Serial.println("updateLEDs5");
+    delay(1000);
+    controlValve(0);
+    Serial.println("controlValve");
+    delay(1000);
+    controlValve(180);
+    Serial.println("controlValve");
+    delay(1000);
+    checkButtonR();
+    Serial.println("checkButtonR");
+    delay(1000);
+    checkButtonC1();
+    Serial.println("checkButtonC1");
+    delay(1000);
+    checkButtonChome();
+    Serial.println("checkButtonChome");
+    delay(1000);
+    checkIRSensor();
+    Serial.println("checkIRSensor");
+    delay(1000);
+    checkRainSensor();
+    Serial.println("checkRainSensor");
+    delay(1000);
+    checkCurrent(CURRENT_PIN1);
+    Serial.println("checkCurrent1");
+    delay(1000);
+    checkCurrent(CURRENT_PIN2);
+    Serial.println("checkCurrent2");
+    delay(1000);
 }
 
